db98377affb600e9e9800960f56e68d74d010fe2
tatm0967005-art <tatm0967005@gmail.com>
2026-05-20 16:39:17 +0000

    Cleanup imported files

--- gundam-store-vn-main/src/components/common/Logo.jsx
+++
@@ -1,9 +0,0 @@
-export default function Logo({ className = "h-12 w-auto" }) {
-  return (
-    <img
-      src="/images/images/logo.png"
-      alt="Gundam Store"
-      className={className}
-    />
-  );
-}
